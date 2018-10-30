211592d553fc746da555ff04002c52561ef10ed3
sugarball <sugarball@foxmail.com>
2018-10-31 01:12:09 +0800

    fix

--- storage.js
+++ storage.js
@@ -1,6 +1,6 @@
 const storage_key = "tailong:luck:key"
 function getStorageAward() {
-  return +localStorage.getItem(storage_key)[0]
+  return (+localStorage.getItem(storage_key) || [0])[0]
 }
 function setStorageAward(award = 0) {
   window.dispatchEvent( new Event('storage') )
